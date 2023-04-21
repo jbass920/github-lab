689ba7a438f849110addead09f6d11214220f246
jbass920 <131471581+jbass920@users.noreply.github.com>
2023-04-21 12:15:28 -0500

    tests
    tests

---
+++ test_account.py
@@ -0,0 +1,25 @@
+import pytest
+
+from account import Account
+
+def test_init():
+    account = Account("John")
+    assert account.get_name() == "John"
+    assert account.get_balance() == 0
+    
+def test_deposit():
+    account = Account("John")
+    assert account.deposit(100) == True
+    assert account.get_balance() == 100
+    assert account.deposit(-50) == False
+    assert account.get_balance() == 100
+    
+def test_withdraw():
+    account = Account("John")
+    assert account.withdraw(50) == False
+    assert account.get_balance() == 0
+    assert account.deposit(100) == True
+    assert account.withdraw(50) == True
+    assert account.get_balance() == 50
+    assert account.withdraw(100) == False
+    assert account.get_balance() == 50
